3c434dc4724b1a61ceaa9a6a1e865f38b32ceb3f
ishika <ishikaasingh2222@gmail.com>
2024-04-01 00:17:47 +0530

    Simulation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Datepicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {FaCalendarAlt} from 'react-icons/fa'
 
+function CustomInput({value,onClick}){
+return(
+  <div className='input-group'>
+    <input type='text' className='form-control' value={value} onClick={onClick} readOnly/>
+    <div className='input-group-append'>
+      <span className='input-group-text'>
+        <FaCalendarAlt/>
+      </span>
+      </div>
+  </div>
+)
+}
 function App() {
+  const [selectedDate,setDate]=useState(null)
+  var[a,b]=useState('Click me')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1 className='header mt-2 mb-4'>Flight Schedule</h1>
+     <label> <Datepicker selected={selectedDate} onChange={date=>setDate(date)} customInput={<CustomInput/>}/>
+     </label>
+     <div className='mt-4 mb-10'>
+      <button onClick={()=>b("Continue")} className='Button'>{a}</button>
+    </div> 
     </div>
-  );
+   
+  )
 }
 
-export default App;
+export default App
